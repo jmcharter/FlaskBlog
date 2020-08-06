ea64879b6c2084dd9f77d17f9f5248cee9b17551
jmcharter <jmcharter@gmail.com>
2020-08-06 22:08:25 +0100

    registration validators

--- app/forms.py
+++ app/forms.py
@@ -15,15 +15,20 @@ class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField('Repeat Password', validators=[DataRequired(),EqualTo('password')])
-    submit = SubmitField('Register'])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(),
+                                       EqualTo('password')])
+    submit = SubmitField('Register')
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
-            raise ValidationError('This username is unavailable, please choose another.')
+            raise ValidationError('This username is unavailable, \
+                                   please choose another.')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
-            raise ValidationError('This email address is already in use, please use another or log in.')
+            raise ValidationError(
+                'This email address is already in use, \
+                 please use another or log in.')
