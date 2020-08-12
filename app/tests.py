ea13fcbed969a6e41b9e364478313f8094828571
jmcharter <jmcharter@gmail.com>
2020-08-12 23:55:53 +0100

    unit tests created

---
+++ app/tests.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+import unittest
+from app import app, db
+from app.models import User, Post
+
+
+class UserModelCase(unittest.TestCase):
+    def setUp(self):
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_password_hashing(self):
+        u = User(username='john')
+        u.set_password('rover11')
+        self.assertFalse(u.check_password('myPassword'))
+        self.assertTrue(u.check_password('rover11'))
+
+    def test_avatar(self):
+        u = User(username='mike', email='mike@flaskblog.org')
+        self.assertEqual(u.avatar(128), ('https://www.gravatar.com/avatar/'
+                                         'd4c74594d841139328695756648b6bd6'
+                                         '?d=identicon&s=128'))
+
+    def test_follow(self):
+        u1 = User(username='mike', email='mike@flaskblog.org')
+        u2 = User(username='john', email='john@example.com')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        self.assertEqual(u1.followed.all(), [])
+        self.assertEqual(u1.followers.all(), [])
+
+        u1.follow(u2)
+        db.session.commit()
+        self.assertTrue(u1.is_following(u2))
+        self.assertEqual(u1.followed.count(), 1)
+        self.assertEqual(u1.followed.first().username, 'john')
+        self.assertEqual(u2.followers.count(), 1)
+        self.assertEqual(u2.followers.first(), 'mike')
+
+        u1.unfollow(u2)
+        db.session.commit()
+        self.assertFalse(u1.is_following(u2))
+        self.assertEqual(u1.followed.count(), 0)
+        self.assertEqual(u2.followers.count(), 0)
+
+    def test_follow_posts(self):
+        
+
+
