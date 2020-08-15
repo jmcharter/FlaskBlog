22aa501f64f40a2f3e2e0a03a8dfebb0868a8d4f
jmcharter <jmcharter@gmail.com>
2020-08-15 19:56:47 +0100

    explore route and pagination added

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,36 @@
 from app import app, db
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm
-from app.models import User
+from app.forms import (LoginForm, RegistrationForm, EditProfileForm, EmptyForm,
+                       PostForm)
+from app.models import User, Post
 from werkzeug.urls import url_parse
 from datetime import datetime
 
 
-@app.route('/')
-@app.route('/index')
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
     form = PostForm()
-    return render_template('index.html', title='Donut Tracker', form=form)
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Post successful.')
+        return redirect(url_for('index'))
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    return render_template('index.html', title='Flask Blog', form=form,
+                           posts=posts.items)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -84,12 +102,6 @@ def edit_profile():
                            form=form)
 
 
-@app.before_request
-def before_request():
-    if current_user.is_authenticated:
-        current_user.last_seen = datetime.utcnow()
-        db.session.commit()
-
 @app.route('/follow/<username>', methods=['POST'])
 @login_required
 def follow(username):
@@ -108,6 +120,7 @@ def follow(username):
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
+
 
 @app.route('/unfollow/<username>', methods=['POST'])
 @login_required
@@ -128,4 +141,10 @@ def unfollow(username):
     else:
         return redirect(url_for('index'))
 
-        
+@app.route('/explore')
+@login_required
+def explore():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    return render_template('index.html', title='Explore', posts=posts.items)
--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['hello@joncharter.co.uk']
+    POSTS_PER_PAGE = 3
