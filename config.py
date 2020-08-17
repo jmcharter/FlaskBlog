fb05327de06d17bff252ba96d8f55fc55000ccf2
jmcharter <jmcharter@gmail.com>
2020-08-17 23:51:19 +0100

    admins variable now pulls from .env

--- config.py
+++ config.py
@@ -12,5 +12,5 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['hello@joncharter.co.uk']
+    ADMINS = os.environ.get('ADMINS')
     POSTS_PER_PAGE = 10
