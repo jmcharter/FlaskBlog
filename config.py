3628c624105dd2795b5db529871ccdc66c9e82bd
Jon Charter <jmcharter@gmail.com>
2020-12-24 02:14:04 +0000

    import dotenv

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
@@ -12,5 +15,5 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = os.environ['ADMINS'].split()
+    ADMINS = os.environ.get('ADMINS')
     POSTS_PER_PAGE = 10
